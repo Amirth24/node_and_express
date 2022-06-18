78d1c8b87156b3def4e68d01793ca037d5cfd40c
Amirth24 <amirth300324@gmail.com>
2022-06-18 19:01:29 +0530

    POST method

--- express-tutorial/Post.js
+++ express-tutorial/Post.js
@@ -4,7 +4,7 @@ const {people} = require('./data');
 
 app.use(express.static('./methods-public'));
 app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+
 // Login Page
 app.get('/login', (req,res) => {
    res.status(200).sendFile(__dirname+'/methods-public/login.html');
@@ -12,9 +12,9 @@ app.get('/login', (req,res) => {
 
 // Handling Login request
 app.post('/login/auth', (req,res) => {
-    const person = findPeople(req.body.name);
-   if(!person) {
-        return  res.status(200).send(`<h1>Hello again, ${person.name}</h1>`);
+    const index = findPeople(req.body.name);
+   if(index != -1) {
+        return  res.status(200).send(`<h1>Hello again, ${people[index].name}</h1>`);
    }
    res.status(401).send(`<h1>You're not an exisiting user.</h1>`) 
 })
@@ -26,25 +26,12 @@ app.post('/add', (req,res)=>{
     if(addPeople(req.body.name)){
         res.status(201).send({success: true, person: req.body.name, msg: 'Successfully added user'});
     }else{
-        res.status(401).send({success: false, msg:'User already Exists'})
+        res.status(401).send({success: false, person: req.body.name, msg:'User already Exists'})
     }
 })
 // Fetching data from the api/person
 app.get('/api/people', (req,res) => {
     res.status(200).json({success: true, data: people})
-});
-
-// PUT method
-app.put('/api/people/:id',(req,res) =>{
-    const {id} = req.params;
-    const {name} = req.body;
-    const person = findPeopleid(id);
-    if(!person){
-        return res.send({sucess: false, person_id: id, msg: 'Please Provide correct id.'})
-    }
-    const old_name = findPeopleid(id).name;
-
-        res.send({success: true, person_id: id, msg: `The person name '${old_name}' is changed to '${name}'`, data:updatePeopleName(id, name)}); 
 })
 
 // express listen
@@ -55,19 +42,10 @@ app.listen(5000,() => {
 
 // Helper Functions
 // Check and return for person if present
-const findPeopleid =(id) => {
-    return people.find((person) => person.id === Number(id));
+const findPeople =(name) => {
+    let found = people.findIndex((data) => name === data.name);
+    return found;
 }
-
-const findPeople = (name) =>{
-    return people.find((person) => person.name === name);
-}
-
-const updatePeopleName = (id,name) =>{
-    const newPeople = people.map((person) =>{if(person.id === id){person.name = name} return person});
-    return newPeople
-}
-
 // Add Person to the api
 const addPeople = (name) => {
     let found = findPeople(name);
